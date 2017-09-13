674b1f7444b646b0978d6922085088936cd4e3a5
Anna Dolan <anna.dolan115@gmail.com>
2017-09-12 21:30:58 -0500

    started earth array

--- javascripts/earth.js
+++ javascripts/earth.js
@@ -1,3 +1,13 @@
 "use strict";
 
 const earthDetails = () =>
+	const thingsAboutEarth () => [{
+		Name: "Earth";
+		YearDiscovered: 
+		Mass:
+		Size:
+		DistanceFromSun:
+		AtmosphereComposition:
+		Satellites:
+		Orbiters:
+	}];
